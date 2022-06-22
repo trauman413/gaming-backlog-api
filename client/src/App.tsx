aa050b97c705b4b8a96d89be7f83ae5d9b2a0559
ksadoff <ksadoff20@gmail.com>
2022-06-21 20:52:15 -0400

    code review feedback

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,21 @@
 import { useState, useEffect } from 'react';
 import Game from './interfaces/Game'
-import 'client/src/styles/game-display.css'
 
 const App = () => {
   let emptyGame: Game = { name: "", platforms: [], genres: [], franchises: [], companies: [], releaseDate: [], summary: "", image: ""}
   const [data, setData] = useState(emptyGame);
 
     useEffect( () => {
-        fetch("/games/supermarioodyssey")
+        fetch(`/games/${emptyGame.name}`)
         .then((res) => res.json())
         .then((data) => setData(data));
-    }, []);
+    }, [emptyGame]);
 
-    const displayMultipleElements = (arr: Array<string>) : string => {
+    const joinStrings = (arr: Array<string>) : string => {
       if (arr.length <= 1) return arr[0]
       let vals = ""
       let count = 0
-      while (count < arr.length) {
-        vals += arr[count] 
-        if (count !== arr.length - 1) {
-          vals += ", "
-        }
-        count++
-      }
+      arr.reduce((acc, cur) => acc ? `${acc}, ${cur}` : cur)
       return vals
     }
 
@@ -35,11 +28,11 @@ const App = () => {
         <div>
         <h1 className="game-title">{data.name}</h1>
         <img src={data.image} alt={data.name} width="150px" height="150px"/>
-        <p><div className="game-field-header">Platforms: </div> {displayMultipleElements(data.platforms)}</p>
-        <p><div className="game-field-header">Franchises: </div> {displayMultipleElements(data.franchises)}</p>
-        <p><div className="game-field-header">Genres: </div> {displayMultipleElements(data.genres)}</p>
-        <p><div className="game-field-header">Companies: </div>{displayMultipleElements(data.companies)}</p>
-        <p><div className="game-field-header">Release: </div> {displayMultipleElements(data.releaseDate)}</p>
+        <p><div className="game-field-header">Platforms: </div> {joinStrings(data.platforms)}</p>
+        <p><div className="game-field-header">Franchises: </div> {joinStrings(data.franchises)}</p>
+        <p><div className="game-field-header">Genres: </div> {joinStrings(data.genres)}</p>
+        <p><div className="game-field-header">Companies: </div>{joinStrings(data.companies)}</p>
+        <p><div className="game-field-header">Release: </div> {joinStrings(data.releaseDate)}</p>
         <br></br>
         <p><div className="game-field-header">Summary: </div> {data.summary}</p>
         </div>
